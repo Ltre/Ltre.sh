8bb62c36321c8aa6e9d77eb46083f46ff1d65590
Ltre Oreki <ltrele@yeah.net>
2021-09-18 10:01:56 +0800

    Update flvsMergeToMp4.sh

--- media/bilibili/BililiveRecorder/flvsMergeToMp4.sh
+++ media/bilibili/BililiveRecorder/flvsMergeToMp4.sh
@@ -2,16 +2,14 @@
 ## 执行此脚本，需先 cd 到存放flv的目录下，且要求flv文件全部都处于当前目录层
 
 safedir=`pwd`/`date +%Y%d%m%H%M%S`
+listfile=$safedir/list.txt
 mkdir $safedir
+echo "" > $listfile
 
 # 按修改时间, 批量复制到一个安全目录并按编号改名
 count=0; for ff in `ls *.flv -rt`; do let count++; cp "$ff" "$safedir/$count.flv"; done;
 
 if [ $count -le 1 ]; then echo 'no flv files'; exit; fi
-
-listfile=$safedir/list.txt
-touch $listfile
-echo "" > $listfile
 
 # 边转换flv->mp4，边生成 [合并专用的配置文件]
 i=1
