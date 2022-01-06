f35f6aca577698501a166537c2fad069929a433f
Ltre Oreki <ltrele@yeah.net>
2022-01-06 18:41:35 +0800

    remote-daemon

--- termux/super-remote-ctrl/init.sh
+++
@@ -1,9 +0,0 @@
-whoami
-#write to .bashrc
-# if no openssh then install it
-# if no golang then install it
-# if no make then install it
-# sshd
-# username=`whoami`
-# frpc -c frpc.ini
-# report {username, sshport, frpc_port, frpc_secret} to Server
