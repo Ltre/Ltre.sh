c38d4110bc7e9965f1c41ee8fea62f5db7fb5585
Ltre Oreki <ltrele@yeah.net>
2022-01-07 11:40:33 +0800

    1

--- termux/loginAndroid.sh
+++ termux/loginAndroid.sh
@@ -1,6 +1,7 @@
 # termux-change-repo，更换三个资源库为 tsinghua（清华大学）
 # pkg i openssh
 # 通过whoami得到用户名为 u0_a309
+# 执行termux-wake-lock
 # 执行sshd启动服务
 # 最后就可以在别的同内网机器建立SSH连接，端口默认8022
 ssh -lu0_a309 -p8022 172.16.15.155
