cd99983589436076415997a40757098bd9a56934
Ltre Oreki <ltrele@yeah.net>
2022-01-28 10:29:08 +0800

    调整音量规划
    音响参数设置
    
    声源  废旧华为p10。
    音响设备  联想56块钱 条状音响。
    音响旋钮调到最大。
    手机蓝牙通过LANGSHI-C3适配器（蓝牙转3.5mm）连接到音响。
    播放基础程序：termux/mpv
    音频文件仅限：新闻联播/焦点访谈
    
    原音量规划：
    
    08:00 - 23:00 （8-11）
    23:00 - 24:00 （8）
    00:00 - 02:00 （6-8）
    02:00 - 08:00 （5-7）
    
    20220128最新音量规划：
    
    09:00 - 23:00 （6-10）
    23:00 - 24:00 （5-7）
    00:00 - 02:00 （3-5）
    02:00 - 09:00 （3-4）

--- termux/audio-anti-thief/maintainVolume.sh
+++ termux/audio-anti-thief/maintainVolume.sh
@@ -2,16 +2,16 @@ while true
 do
     #echo \[`date +%Y-%M-%D %H%m%S`\]
     echo 'waiting next volume for 60s...'
-    sleep 60
+    sleep 75
     # date +%H%M%S
-    if [ `date +%k` -ge 8 ] && [ `date +%k` -lt 23 ]; then
-        termux-volume music $((8 + $RANDOM%4)) # 08:00~23:00 音量(8~11)/15
+    if [ `date +%k` -ge 9 ] && [ `date +%k` -lt 23 ]; then
+        termux-volume music $((6 + $RANDOM%5)) # 09:00~23:00 音量(6~10)/15
     elif [ `date +%k` -ge 23 ] && [ `date +%k` -ne 0 ]; then
-        termux-volume music 8 # 23:00~00:00 音量8/15
+	termux-volume music $((5 + $RANDOM%3)) # 23:00~00:00 音量(5-7)/15
     elif [ `date +%k` -ge 0 ] && [ `date +%k` -lt 2 ]; then
-        termux-volume music $((6 + $RANDOM%3)) # 00:00~02:00 音量(6~8)/15
+        termux-volume music $((3 + $RANDOM%3)) # 00:00~02:00 音量(3~5)/15
     else
-        termux-volume music $((5 + $RANDOM%3)) # 02:00~08:00 音量(5~7)/15
+        termux-volume music $((3 + $RANDOM%2)) # 02:00~09:00 音量(3-4)/15
     fi
 done
 
