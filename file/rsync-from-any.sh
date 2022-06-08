2d15aab0d4817263e2398b4ccf3679696ea87632
Ltre Oreki <ltrele@yeah.net>
2022-06-08 18:15:28 +0800

    1

--- file/rsync-from-any.sh
+++ file/rsync-from-any.sh
@@ -4,4 +4,4 @@ HOST=$1
 PORT=$2
 REMOTEPATH=$3
 LOCALPATH=$4
-sshpass -p "远程机器密码" rsync -av -e "ssh -p ${PORT}" abc@${HOST}:${REMOTEPATH}  "${LOCALPATH}"
+sshpass -p "远程机器密码" rsync -av -e "ssh -p ${PORT}" 用户名@${HOST}:${REMOTEPATH}  "${LOCALPATH}"
