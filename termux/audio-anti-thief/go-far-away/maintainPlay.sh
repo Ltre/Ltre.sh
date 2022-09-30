153d371514b94ac9d712e814268b18a1e62e937a
Ltre Oreki <ltrele@yeah.net>
2022-09-30 14:53:43 +0800

    Update maintainPlay.sh

--- termux/audio-anti-thief/go-far-away/maintainPlay.sh
+++ termux/audio-anti-thief/go-far-away/maintainPlay.sh
@@ -1,9 +1,12 @@
 
 playAudio(){
+    if [ "`ps aux|grep mpv|grep -wv grep|awk '{print $2}'`" ]; then
+        return
+    fi
     CUR_DIR=$(cd `dirname $0` && pwd -P)
-    songpath=`ls ${CUR_DIR}/../audio/*.m4a | sort -R | head -n1`
+    songpath=`ls ${CUR_DIR}/../audio/*.{m4a,webm,mp3,aac,wma} 2>/dev/null | sort -R | head -n1`
     echo "Playing ..."
-    echo $songpath
+    echo "$songpath"
     mpv "$songpath"
 }
 
@@ -22,6 +25,9 @@ resumeAudio(){
 
 while true
 do
+    if [ `date +%S` -ge 10 ] && [ `date +%S` -le 12 ]; then
+        echo "Current volume of music: "`termux-volume|jq ".[3].volume"`;
+    fi
     echo 'waiting next track for 2s...'
     sleep 2
     playAudio
