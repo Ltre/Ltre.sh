14577d7806532cd62389280db14d204567d3e787
Ltre Oreki <Ltre@users.noreply.github.com>
2022-11-01 14:22:19 +0800

    启动frpc进程常驻，不含敏感信息

---
+++ termux/super-remote-ctrl/frpc-enable-public.sh
@@ -0,0 +1,5 @@
+# 启动frpc进程常驻，不含敏感信息
+frpcPID=`ps -ef|grep frpc.ini|grep -v grep|awk '{print $2}'`
+if [ "$frpcPID" = "" ]; then
+    nohup ~/bin/frp/frpc -c ~/bin/frp/conf/frpc.ini >> ~/bin/frp/frpc.log &
+fi
