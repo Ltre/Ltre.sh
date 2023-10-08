6a0aacf74a6f1276198c8291b082823f1db87c2e
Ltre Oreki <Ltre@users.noreply.github.com>
2023-10-09 00:38:27 +0800

    批量将同层目录内的apk带密码打包成zip，防止gDrive误识别为病毒文件

---
+++ termux/apk/apk2zip.tcwfpy.php
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/php
+<?php
+
+$files = glob('*.apk'); //在centos7/8中证实，glob拿到的列表顺序，跟ls -l得到的无异
+$zip = new ZipArchive;
+foreach ($files as $v) {
+  $to = $v.'.tcwfpy.zip';
+  $cmd = "zip -r -P 密码 {$to} {$v}";
+  system($cmd);
+  unlink($v);
+}
