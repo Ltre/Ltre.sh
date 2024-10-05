31c2ec348974ad281fa8358ef08c03e3c445f611
Ltre Oreki <Ltre@users.noreply.github.com>
2024-10-05 14:18:45 +0800

    Update anyproxy.sh

--- network/anyproxy.sh
+++ network/anyproxy.sh
@@ -26,9 +26,18 @@ case "$1" in
         shift
         pip --proxy=$proxy "$@"
         ;;
+    pip3)
+        shift
+        pip3 --proxy=$proxy "$@"
+        ;;
     npm)
         shift
         npm --proxy=$proxy "$@"
+        ;;
+    composer)
+        shift
+        composer config -g repositories.packagist composer $proxy
+        composer "$@"
         ;;
     apt-get)
         shift
@@ -53,6 +62,70 @@ case "$1" in
     sftp)
         shift
         sftp -o "ProxyCommand=nc -x 127.0.0.1:1080 %h %p" "$@"
+        ;;
+    rails)
+        shift
+        rails new myapp --skip-bundle
+        cd myapp
+        bundle config set --local mirror.https://rubygems.org $proxy
+        bundle install
+        ;;
+    flutter)
+        shift
+        flutter pub get --proxy=$proxy
+        ;;
+    mvn)
+        shift
+        mvn -Dproxy.host=127.0.0.1 -Dproxy.port=1080 "$@"
+        ;;
+    lein)
+        shift
+        lein with-proxy "$@"
+        ;;
+    go)
+        shift
+        GOPROXY=$proxy go "$@"
+        ;;
+    yarn)
+        shift
+        yarn config set proxy $proxy
+        yarn "$@"
+        ;;
+    brew)
+        shift
+        HOMEBREW_PROXY=$proxy brew "$@"
+        ;;
+    pkg)
+        shift
+        pkg install --proxy=$proxy "$@"
+        ;;
+    ftp)
+        shift
+        ftp -p -o "http_proxy=$proxy" "$@"
+        ;;
+    curlftpfs)
+        shift
+        curlftpfs -o "proxy=$proxy" "$@"
+        ;;
+    httpie)
+        shift
+        http --proxy=$proxy "$@"
+        ;;
+    mtr)
+        shift
+        mtr --proxy=$proxy "$@"
+        ;;
+    ssh)
+        shift
+        ssh -o ProxyCommand='nc -x 127.0.0.1:1080 %h %p' "$@"
+        ;;
+    socat)
+        shift
+        socat - SOCKS5:127.0.0.1:$@ 
+        ;;
+    gpg)
+        shift
+        gpg --keyserver hkp://keyserver.ubuntu.com:80 --proxy=$proxy "$@"
         ;;
     *)
         echo "Unsupported command: $1"
