8c37c68b68c78729ea122482a1e7f27b3a094002
Ltre Oreki <Ltre@users.noreply.github.com>
2024-10-11 14:33:03 +0800

    Update pushCsh2Txhk.sh

--- network/pushCsh2Txhk.sh
+++ network/pushCsh2Txhk.sh
@@ -1,7 +1,8 @@
-# 用于发布订阅链接，不能直接使用
+# 用于发布订阅链接，代码修改成正确的才能使用
 HOST=x-aa-bb.adb.com
 PORT=8999
 LOCALPATH=/sdcard/Download/Sync/工作空间专用/FQ/ccc.txt
 REMOTEPATH=/home/wwwroot/ccc.adb.com/ccc/dontcopycccname.txt
 PASS=`cat ~/pehz/shs`
 sshpass -p "$PASS" rsync -avP -e "ssh -p ${PORT}"  "${LOCALPATH}"  root@${HOST}:"${REMOTEPATH}"
+sshpass -p "$PASS" ssh -lroot -p$PORT $HOST "$rmtShel"
